b08e62881f2c4b06ec153e1447389cbf53e49dae
Specnr <specnrr@gmail.com>
2021-06-14 12:51:08 -0400

    remove previous world save, unneeded

--- reset.sh
+++ reset.sh
@@ -1,12 +1,8 @@
 ARGS=$(cat jvmArgs.txt)
-# Remove world files
-rm -rf prev_world*
-echo "World Deleted"
-# Keep previous world for verification
-mv world prev_world
-echo "Backed up previous world"
+
 # Edit seed in server.properties
 python3 ./updateProperties.py $1
-echo "Loaded Seed " $1
+[ $# -gt 0 ] && echo "Loaded Seed " $1
+
 # Start server
 java $ARGS -jar server.jar nogui .
