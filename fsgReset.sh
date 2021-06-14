dc3bcf1c7b4ba3ab01e28e05c63ba70fb071c67a
Specnr <specnrr@gmail.com>
2021-06-14 16:59:37 -0400

    added rm world

--- fsgReset.sh
+++ fsgReset.sh
@@ -2,8 +2,9 @@ ARGS=$(cat jvmArgs.txt)
 echo "Generating seed..."
 seed=$(python3 findSeed.py)
 echo "Seed Found"
+
 # Edit seed in server.properties
 python3 ./updateProperties.py $seed
-
+rm -rf world*
 # Start server
 java $ARGS -jar server.jar nogui .
--- reset.sh
+++ reset.sh
@@ -3,6 +3,6 @@ ARGS=$(cat jvmArgs.txt)
 # Edit seed in server.properties
 python3 ./updateProperties.py $1
 [ $# -gt 0 ] && echo "Loaded Seed " $1
-
+rm -rf world*
 # Start server
 java $ARGS -jar server.jar nogui .
